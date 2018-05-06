31a1610a8f588141992402a7b65b07e0cf213f94
Archeth <archeth.r@gmail.com>
2018-05-06 22:08:25 +0700

    initial commit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 num, num_1;
+            Console.WriteLine("Enter first number: ");
+            num = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("\nEnter second number: ");
+            num_1 = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("The result is: " + (num + num_1).ToString());
+            Console.ReadKey();
+
+
+        }
+    }
+}
