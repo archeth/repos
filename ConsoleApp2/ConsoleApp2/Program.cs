424f3e87f56774aecf255118c9904902771b020b
Archeth <archeth.r@gmail.com>
2018-05-06 23:40:14 +0700

    initial commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 x, n;
+            int y = 1;
+            Console.WriteLine("Введите x: ");
+            x = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Введите степень: ");
+            n = Convert.ToInt64(Console.ReadLine());
+            if (n == 0)
+            {
+                Console.WriteLine(x + " в степени " + n + " = " + y);
+            }
+            else
+            { Console.WriteLine(x+" в степени "+n+" = "+Math.Pow(x,n)); }
+            Console.ReadKey();
+
+        }
+    }
+}
