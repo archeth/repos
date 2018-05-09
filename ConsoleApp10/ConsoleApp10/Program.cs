22eb12843777dcac7c2cce8955101a3b81909951
Archeth <archeth.r@gmail.com>
2018-05-09 23:47:34 +0700

    initial commit

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");
+            String words = Console.ReadLine();
+            String[] arr = words.Split(new char[] { ',', '.', '!', '?', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("Количество слов в тексте: {0}", arr.Length);
+            Console.ReadKey();
+        }
+    }
+}
