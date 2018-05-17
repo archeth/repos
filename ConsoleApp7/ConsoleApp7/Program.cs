e5036e1a82905a3b0b51ba9fcc23f67814c94a4f
archeth <archeth.r@gmail.com>
2018-05-18 00:19:32 +0700

    Update Program.cs

--- ConsoleApp7/ConsoleApp7/Program.cs
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -10,14 +10,12 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            long number, typeid, cityid, tableid, recordid;
+           long number, typeid, cityid, tableid, recordid;
             number = 1267165676175383;
-            byte number1 = 0;
-            number1 = System.Convert.ToByte(number);
-            typeid = number >> 2;
-            cityid = typeid >> 13;
-            tableid = cityid >> 15;
-            recordid = tableid >> 32;
+            typeid = number >> 49;
+            cityid = number >> 38;
+            tableid = number >> 36;
+            recordid = number >> 19;
             Console.WriteLine("Type ID is:" + typeid);
             Console.WriteLine("City ID is:" + cityid);
             Console.WriteLine("Table ID is:" + tableid);
