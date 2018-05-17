69799703b49784a360c8f80e72f71319ba7f6884
Archeth <archeth.r@gmail.com>
2018-05-18 01:18:50 +0700

    fix2

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -10,49 +10,36 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            Int64 n;
+            long n;
+            var matrix1 = string.Empty;
+            var matrix2 = string.Empty;
+            var matrix3 = string.Empty;
             Console.WriteLine("Введите N ");
             n = Convert.ToInt64(Console.ReadLine());
-            int[,] matrix1 = new int[n, n];
-            Console.WriteLine("Матрица 1: ");
-            Random random1 = new Random();
-            int rand1;
+            int[,] matrixRandom = new int[n, n];
+            int[,] matrixSUM = new int[n, n];
+            Random random = new Random();
+            int rand;
             for (int i = 0; i < n; i++)
+
             {
                 for (int j = 0; j < n; j++)
+
                 {
-                    rand1 = random1.Next(0, 100);
-                    matrix1[i, j] = rand1;
-                    Console.Write("{0}\t", matrix1[i, j]);
+                    rand = random.Next(0, 100);
+                    matrixRandom[i, j] = rand;
+                    matrix1 = matrix1 + $"{matrixRandom[i, j]}\t";
+                    matrix2 = matrix2 + $"{matrixRandom[i, j]}\t";
+                    matrixSUM[i, j] = matrixRandom[i, j] + matrixRandom[i, j];
+                    matrix3 = matrix3 + $"{matrixSUM[i, j]}\t";
+
                 }
-                Console.WriteLine();
+                
             }
 
-            int[,] matrix2 = new int[n, n];
-            Console.WriteLine("Матрица 2: ");
-            Random random2 = new Random();
-            int rand2;
-            for (int x = 0; x < n; x++)
-            {
-                for (int y = 0; y < n; y++)
-                {
-                    rand2 = random2.Next(0, 100);
-                    matrix2[x, y] = rand2;
-                    Console.Write("{0}\t", matrix2[x, y]);
-                }
-                Console.WriteLine();
-            }
-            int[,] matrixSUM = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
-            Console.WriteLine("Сумма матриц: ");
-            for (int c = 0; c < matrix1.GetLength(0); c++)
-            {
-                for (int z = 0; z < matrix2.GetLength(1); z++)
-                {
-                    matrixSUM[c, z] = matrix1[c, z] + matrix2[c, z];
-                    Console.Write("{0}\t", matrixSUM[c, z]);
-                }
-                Console.WriteLine();
-            }       
+            Console.WriteLine("Матрица 1: " +matrix1);
+            Console.WriteLine("Матрица 2: " + matrix2);
+            Console.WriteLine("Сумма    : " + matrix3);
             Console.ReadKey();
         }
     }
