21ce21eaaaafe1e0fb29f19025f6dc56f137e7ea
Archeth <archeth.r@gmail.com>
2019-06-16 20:39:19 +0300

    [NA]

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
@@ -17,29 +13,24 @@ namespace ConsoleApp5
             Console.WriteLine("Введите N ");
             n = Convert.ToInt64(Console.ReadLine());
             int[,] matrixRandom = new int[n, n];
-            int[,] matrixSUM = new int[n, n];
+            int[,] matrixMLT = new int[n, n];
             Random random = new Random();
             int rand;
             for (int i = 0; i < n; i++)
-
             {
                 for (int j = 0; j < n; j++)
-
                 {
                     rand = random.Next(0, 100);
                     matrixRandom[i, j] = rand;
                     matrix1 = matrix1 + $"{matrixRandom[i, j]}\t";
                     matrix2 = matrix2 + $"{matrixRandom[i, j]}\t";
-                    matrixSUM[i, j] = matrixRandom[i, j] + matrixRandom[i, j];
-                    matrix3 = matrix3 + $"{matrixSUM[i, j]}\t";
-
+                    matrixMLT[i, j] = matrixRandom[i, j] * matrixRandom[i, j];
+                    matrix3 = matrix3 + $"{matrixMLT[i, j]}\t";
                 }
-                
             }
-
-            Console.WriteLine("Матрица 1: " +matrix1);
-            Console.WriteLine("Матрица 2: " + matrix2);
-            Console.WriteLine("Сумма    : " + matrix3);
+            Console.WriteLine("First matrix: " +matrix1);
+            Console.WriteLine("Second matrix: " + matrix2);
+            Console.WriteLine("Multiply result: " + matrix3);
             Console.ReadKey();
         }
     }
