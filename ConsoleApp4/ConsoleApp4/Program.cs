3e8782ec98c8ed6fa67ce4b35b59df118716a14c
Archeth <archeth.r@gmail.com>
2019-06-16 20:30:07 +0300

    [NA]

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -14,10 +10,38 @@ namespace ConsoleApp1
             var number1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("\nEnter second number: ");
             var number2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("The result is: " + (number1 + number2).ToString());
+            Console.WriteLine(@"Select the arithmetic operation:
+            - addition (enter 1)
+            - subtraction (enter 2)
+            - multiply (enter 3)
+            - division (enter 4)");
+            var operation = Convert.ToInt32(Console.ReadLine());
+
+            if (operation == 1)
+            {
+                Console.WriteLine("The result is {0} ", number1 + number2);
+            }
+            if (operation == 2)
+            {
+                Console.WriteLine("The result is {0} ", number1 - number2);
+            }
+            if (operation == 3)
+            {
+                Console.WriteLine("The result is {0} ", number1 * number2);
+            }
+            if (operation == 4)
+            {
+                if (number2 == 0)
+                {
+                    Console.WriteLine("Division by zero is disallowed");
+                }
+                else
+                {
+                    Console.WriteLine("The result is {0} ", number1 / number2);
+                }
+            }
+            
             Console.ReadKey();
-
-
         }
     }
 }
